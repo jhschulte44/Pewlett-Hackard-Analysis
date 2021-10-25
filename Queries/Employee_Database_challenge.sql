9720fd1e36ccd6d3f302de491d079be76bef1c79
Jack Key <jhschulte44@gmail.com>
2021-10-24 20:37:29 -0400

    move .sql files to queries folder

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,61 @@
+-- Create new table with retiree titles 
+SELECT e.emp_no,
+e.first_name,
+e.last_name,
+t.title,
+t.from_date,
+t.to_date
+
+INTO retirement_titles
+FROM employees AS e
+JOIN titles AS t
+ON (e.emp_no=t.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no;
+
+SELECT * FROM retirement_titles;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
+
+SELECT * FROM unique_titles;
+
+-- Calculate the number of employees about to retire
+SELECT title, COUNT (title) AS "retiree count"
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY "retiree count" DESC;
+
+SELECT * FROM retiring_titles;
+
+-- Create a Mentorship Eligibility table
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+t.title
+
+INTO mentorship_eligibility
+FROM employees AS e
+JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE de.to_date = ('9999-01-01')
+AND (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
+
+SELECT * FROM mentorship_eligibility;
+
+
+
